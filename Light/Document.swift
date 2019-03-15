b83a485895e5e342e0851b77ab3e8acc3a45b9db
Kevin <Kevin_baba@naver.com>
2019-03-15 14:03:19 +0900

    initial commit

---
+++ Light/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  Light
+//
+//  Created by Kevin Kim on 15/03/2019.
+//  Copyright © 2019 Piano. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
